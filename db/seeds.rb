119ca27320f77ca67fca5cad941ab5dc22d89767
Cataline Baert <snuf_kin@yahoo.com>
2021-02-16 21:02:01 +0100

    Seed 6 flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+puts 'Cleaning database...'
+Flat.destroy_all
+puts 'Done'
+
+puts 'Creating 6 flats...'
+url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+flats = JSON.parse(open(url).read)
+flats.each do |flat|
+  Flat.create!(
+    name: flat['name'],
+    image_url: flat['imageUrl'],
+    price: flat['price'],
+    currency: flat['priceCurrency']
+  )
+end
+puts "Seeded #{Flat.count} flats"
+
+puts 'Finished!'
